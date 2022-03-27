e9cc9af963f4ebdb48034e5345f1fa5c8f45a038
Jonás Márquez <jonas@jonasmarquez.com>
2022-03-27 18:37:38 +0200

    Update module

--- bucket.tf
+++ bucket.tf
@@ -4,24 +4,21 @@
 resource "aws_s3_bucket" "s3-bucket" {
   bucket = var.bucket_name # IMPORTANT! Conflict with bucket_prefix
   #bucket_prefix = var.bucket_prefix # IMPORTANT! Conflict with bucket
-  #acl = "private"
-  # Versioning Configuration
-  /*versioning {
-    enabled = var.versioning
-  }*/
-
-  # Encryption Configuracion for S3 Bucket (needs KMS Key)
-  /*server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = var.kms_master_key_id
-        sse_algorithm = var.sse_algorithm
-      }
-    }
-  }*/
 
   # TAGs of Resources
   tags = var.tags
+}
+
+resource "aws_s3_bucket_acl" "s3-acl" {
+  bucket = aws_s3_bucket.s3-bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "s3-versioning" {
+  bucket = aws_s3_bucket.s3-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "s3-bucket-acl" {
