13270ebbba0b84d2f963dac664cf8b8800c8742d
Jonás Márquez <jonas@jonasmarquez.com>
2022-03-27 18:21:00 +0200

    Disable ACL

--- bucket.tf
+++ bucket.tf
@@ -4,9 +4,9 @@
 resource "aws_s3_bucket" "s3-bucket" {
   bucket = var.bucket_name # IMPORTANT! Conflict with bucket_prefix
   #bucket_prefix = var.bucket_prefix # IMPORTANT! Conflict with bucket
-  acl = "private"
-   # Versioning Configuration
-   versioning {
+  #acl = "private"
+  # Versioning Configuration
+  versioning {
     enabled = var.versioning
   }
 
