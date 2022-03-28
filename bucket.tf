e189494b2722bb8ee5f2a3e5b574c7a025561aee
Jonás Márquez <jonas@jonasmarquez.com>
2022-03-28 20:35:51 +0200

    Add provider block

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,10 @@
 #------------------
 # AWS RESOURCE S3
 #------------------
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_s3_bucket" "s3-bucket" {
   #bucket = var.bucket_name # IMPORTANT! Conflict with bucket_prefix
   bucket_prefix = var.bucket_prefix # IMPORTANT! Conflict with bucket
