3e5a0ced9d4730ed447fb9668e0f3ae4b8770edb
Jonás Márquez <jonas@jonasmarquez.com>
2022-03-28 22:50:53 +0200

    Update bucket name vars

--- bucket.tf
+++ bucket.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "s3-bucket" {
   bucket = var.bucket_name # IMPORTANT! Conflict with bucket_prefix
-  bucket_prefix = var.bucket_prefix # IMPORTANT! Conflict with bucket
+  #bucket_prefix = var.bucket_prefix # IMPORTANT! Conflict with bucket
 
   # TAGs of Resources
   tags = var.tags
